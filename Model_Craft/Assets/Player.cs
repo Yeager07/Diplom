b96bbedb2869507c1ebd3b6df04899d520a9e216
Garek <Igor.Esipenko@urfu.me>
2026-01-05 17:19:53 +0500

    Реализовал перемещение в режиме сборки

--- Model_Craft/Assets/Player.cs
+++ Model_Craft/Assets/Player.cs
@@ -10,6 +10,8 @@ public class Player1 : MonoBehaviour
     private Rigidbody rigidBody;
     private Vector3 moveDirection;
     private Vector3 rotateDirection;
+    public bool isBuildMode = false;
+    public GameObject target;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,14 +30,47 @@ public class Player1 : MonoBehaviour
         if(rotateDirection.x > verRotLim)
         rotateDirection.x = verRotLim;
 
-        moveDirection = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
-        rigidBody.MovePosition(rigidBody.position + moveDirection * speed * Time.deltaTime);
         transform.rotation = Quaternion.Euler(rotateDirection);
+    }
+    
+    void MoveBuildMode()
+    {
+        rigidBody.constraints = RigidbodyConstraints.FreezeRotationZ;
+        GetComponent<MeshRenderer>().enabled = false;
+        
+        moveDirection = transform.forward * Input.GetAxis("Mouse ScrollWheel") * 100;
+            
+        if(Input.GetMouseButton(2))
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            moveDirection = -transform.up * Input.GetAxis("Mouse Y") - transform.right * Input.GetAxis("Mouse X");
+
+            else
+            Move();
+            /*{
+                transform.RotateAround(target.transform.position, Vector3.up, Input.GetAxis("Mouse X"));
+                transform.RotateAround(target.transform.position, Vector3.right, Input.GetAxis("Mouse Y"));
+            }*/
+        }
+
+        rigidBody.MovePosition(rigidBody.position + moveDirection * speed * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if(!isBuildMode)
+        {
+            rigidBody.constraints = RigidbodyConstraints.FreezePositionY;
+            GetComponent<MeshRenderer>().enabled = true;
+
+            Move();
+
+            moveDirection = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+            rigidBody.MovePosition(rigidBody.position + moveDirection * speed * Time.deltaTime);
+        }
+
+        else
+        MoveBuildMode();
     }
 }
